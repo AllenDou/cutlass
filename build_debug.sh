e0c9218139caaa36fcf6c6fcc3cf064ed9fabd80
zixiao <shunli.dsl@alibaba-inc.com>
2024-11-11 15:51:43 +0800

    update.

--- build_debug.sh
+++ build_debug.sh
@@ -1,1 +1,3 @@
+echo "-DCUTLASS_NVCC_ARCHS=89 for ampere"
+echo "-DCUTLASS_NVCC_ARCHS=90a for hopper"
 mkdir -p build && cd build && cmake .. -DCUTLASS_NVCC_ARCHS=89 -DCMAKE_BUILD_TYPE=Debug -DCUTLASS_CUDA_NVCC_FLAGS=-G
